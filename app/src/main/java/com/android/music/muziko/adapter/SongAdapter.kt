296ddf2f6620d864c1793fa9b8c51b1678c60f4b
huypham85 <huypham8501@gmail.com>
2021-10-24 22:17:31 +0700

    add song adapter and song viewModel

---
+++ app/src/main/java/com/android/music/muziko/adapter/SongAdapter.kt
@@ -0,0 +1,47 @@
+package com.android.music.ui
+
+import android.app.Activity
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.android.music.databinding.ItemSongBinding
+import com.example.kookplayer.utlis.ImageUtils
+
+class SongAdapter(var listSong : ArrayList<Song>,val context: Activity) : RecyclerView.Adapter<SongAdapter.SongViewHolder>() {
+    var position = 0
+    inner class SongViewHolder(private var binding: ItemSongBinding): RecyclerView.ViewHolder(binding.root){
+        var title = binding.txtTitle
+        var artist = binding.txtArtist
+        var imageSong = binding.imgSong
+        var menuBtn = binding.musicMenuBtn
+        fun bind(song:Song){
+            title.text = song.title
+            artist.text = song.artist
+            song.image?.let {
+                ImageUtils.loadImageToImageView(
+                    context = context,
+                    imageView = imageSong,
+                    image = it
+                )
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongAdapter.SongViewHolder {
+        var binding = ItemSongBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return SongViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SongAdapter.SongViewHolder, position: Int) {
+        val song = listSong[position]
+        this.position = position
+        holder.apply {
+            bind(song)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listSong.size
+    }
+}
