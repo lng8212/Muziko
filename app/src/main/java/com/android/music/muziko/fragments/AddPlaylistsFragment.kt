7c93bdc81cde971bf0f958fe53f6a26ff882dfd8
DIEPDB <diepdao1708@gmail.com>
2021-10-28 00:25:07 +0700

    add playlists

--- app/src/main/java/com/android/music/muziko/fragments/AddPlaylistsFragment.kt
+++ app/src/main/java/com/android/music/muziko/fragments/AddPlaylistsFragment.kt
@@ -5,16 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.android.music.R
+import com.android.music.databinding.FragmentAddPlaylistsBinding
 
 class AddPlaylistsFragment : Fragment() {
+
+    private lateinit var binding: FragmentAddPlaylistsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_playlists, container, false)
+        binding = FragmentAddPlaylistsBinding.inflate(inflater, container, false)
+
+        binding.txtCancelAddPlaylistsFragment.setOnClickListener {
+            findNavController().navigate(R.id.action_addPlaylistsFragment_to_playlistsFragment)
+        }
+
+        binding.txtDoneAddPlaylistsFragment.setOnClickListener {
+            findNavController().navigate(R.id.action_addPlaylistsFragment_to_playlistsFragment)
+        }
+
+        binding.constraintAddMusicAddFragment.setOnClickListener {
+
+        }
+        return binding.root
     }
 
 }
--- app/src/main/java/com/android/music/muziko/fragments/LibraryFragment.kt
+++ app/src/main/java/com/android/music/muziko/fragments/LibraryFragment.kt
@@ -5,15 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.android.music.R
+import com.android.music.databinding.FragmentLibraryBinding
 
 class LibraryFragment : Fragment() {
+
+    private lateinit var binding: FragmentLibraryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_library, container, false)
+        binding = FragmentLibraryBinding.inflate(inflater, container, false)
+
+        binding.layoutPlaylistsLibrary.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_library_to_playlistsFragment)
+        }
+
+        binding.layoutArtistsLibrary.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_library_to_artistsFragment)
+        }
+        return binding.root
     }
 }
