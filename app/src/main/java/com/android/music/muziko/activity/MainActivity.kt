335459fb8fe355d3571db9e0be2c96e10b46c567
Long <long05012001@gmail.com>
2021-11-04 15:23:17 +0700

    fix favor

--- app/src/main/java/com/android/music/muziko/activity/MainActivity.kt
+++ app/src/main/java/com/android/music/muziko/activity/MainActivity.kt
@@ -78,6 +78,7 @@ class MainActivity : AppCompatActivity() {
         checkForPermissions()
 
         binding.layoutOnCollapsed.setOnClickListener {
+            RoomRepository.updateCachedFav()
             val intent = Intent(this, PlayerPanelActivity::class.java)
             startActivity(intent)
 
