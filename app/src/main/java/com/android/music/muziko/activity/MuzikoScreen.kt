3127474d3e96c7abcd8a910b36430cbc726d43ec
Long <long05012001@gmail.com>
2022-03-20 00:23:42 +0700

    splash screen

--- app/src/main/java/com/android/music/muziko/activity/MuzikoScreen.kt
+++ app/src/main/java/com/android/music/muziko/activity/MuzikoScreen.kt
@@ -3,7 +3,6 @@ package com.android.music.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.android.music.R
 import com.android.music.muziko.activity.MainActivity
@@ -15,23 +14,10 @@ class MuzikoScreen : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-
-
-        setContentView(R.layout.screen_muziko)
-
-        // This is used to hide the status bar and make
-        // the splash screen as a full screen activity.
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-
-        // used the postDelayed(Runnable, time) method
-        // to send a message with a delayed time.
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000) // in milliseconds.
+        }, 1000)
     }
 }
